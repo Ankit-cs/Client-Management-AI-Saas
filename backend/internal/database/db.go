8f1d98cd05cab260a6d540247f619173aacd477d
Ankit-cs <ankitcareer018@gmail.com>
2026-05-26 21:37:22 +0530

    completed User Schema

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -6,22 +6,17 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool" //to establish connection with postgresSql
 )
 
-
+//global context helps in timeouts and cancellation of database operations and closing any connections
 func NewPool(ctx context.Context, databaseURL string) (*pgxpool.Pool, error) {
 
 	pool,err:=pgxpool.New(ctx,databaseURL)
 	if err!=nil{
 		return nil,fmt.Errorf("unable to create connection pool: %v",err)
 	}
-
 	if err:=pool.Ping(ctx);err!=nil{
 		pool.Close()
 		return nil,fmt.Errorf("unable to connect to database: %v",err)
 	}
 	return pool,nil
-
-
-
-
 
 }
