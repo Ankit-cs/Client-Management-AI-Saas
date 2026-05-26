24be22aae1bbf085f7cc6e0871ef174ecc4ffda5
Ankit-cs <ankitcareer018@gmail.com>
2026-05-26 21:44:55 +0530

    completed migration of database for every new context of teh db in cmd/migrate/main.go

---
+++ backend/cmd/migrate/main.go
@@ -0,0 +1,20 @@
+package main // this is going to use logic of migrator.go file
+import (
+	"backend/internal/config"
+	"backend/internal/database"
+	"context"
+	"log"
+)
+func main() {
+	// we will call the function to run the migration
+	cfg, err := config.Load();
+	if err != nil {
+		log.Fatalf("Failed to load configuration: ", err)
+	}
+	ctx:=context.Background()//it is creating empty context
+	db,err:=database.NewPool(ctx,cfg.DatabaseURL)
+	if err!=nil{
+	log.Fatalf("Failed to connect to database: ", err)
+	}
+	defer db.Close()//it is closing the connection to database after the migration is done
+}
