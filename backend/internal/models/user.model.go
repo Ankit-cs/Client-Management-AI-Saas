a2c9459c4691bbb7baad35b2700c2b09fd2874e7
Ankit-cs <ankitcareer018@gmail.com>
2026-05-27 12:02:29 +0530

    created all the important files needed for creating router,services

---
+++ backend/internal/models/user.model.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+)	
+//user struct can be compare with mongoose schema
+type User struct{
+	Id string `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Avatar string `json:"avatar"`
+	Role string `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+} 
+//we have to create repository layer
+//repositry interact with database and tehn we create services layer
+//service interact with handler
+//then we create httpHandlers create handler files and route files 4 files total
+//user handler
