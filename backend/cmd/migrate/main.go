ac4c75f6a6bc8df4090afd5c26f9c2b117fc3022
Ankit-cs <ankitcareer018@gmail.com>
2026-05-27 11:15:07 +0530

    feat:solved bug related to config file

--- backend/cmd/migrate/main.go
+++ backend/cmd/migrate/main.go
@@ -4,19 +4,23 @@ import (
 	"backend/internal/database"
 	"context"
 	"log"
+	//"os"
 )
 func main() {
 	// we will call the function to run the migration config our env variables 
 	cfg, err := config.Load();
 	if err != nil {
-		log.Fatalf("Failed to load configuration: ", err)
+		log.Fatalf("Failed to load configuration: %v", err)
 	}
 	ctx:=context.Background()//it is creating empty context
 	db,err:=database.NewPool(ctx,cfg.DatabaseURL)
 	if err!=nil{
-	log.Fatalf("Failed to connect to database: ", err)
+	log.Fatalf("Failed to connect to database: %v", err)
 	}
 	defer db.Close()//it is closing the connection to database after the migration is done when the main function finishes
 	//this runs as the end of the function
-	
+	if err=database.RunMigrations(ctx,db);err!=nil{
+		log.Fatalf("run migrations: %v", err)
+	}
+	log.Panicln("Migrations completed successfully")
 }
