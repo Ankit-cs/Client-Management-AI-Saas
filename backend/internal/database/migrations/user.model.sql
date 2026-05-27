fa1e6400847f11cd48b96fbedc81282c3e0e8018
Ankit-cs <ankitcareer018@gmail.com>
2026-05-27 11:36:52 +0530

    feat:solve bug in user.model file

--- backend/internal/database/migrations/user.model.sql
+++ backend/internal/database/migrations/user.model.sql
@@ -1,10 +1,9 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;//unique uuid  extension
-
-CREATE TABLE IF NOT EXISTS users{
+CREATE EXTENSION IF NOT EXISTS pgcrypto; -- unique uuid extension
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL DEFAULT 'user' CHECK (role IN ('user', 'admin')),
-    created_at TIMESTAMP NOT NULL  DEFAULT NOW(),
-}
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
