9493914119115dcde687aa38bc7efd90a7490362
Ankit-cs <ankitcareer018@gmail.com>
2026-05-28 16:56:09 +0530

    completed api/main.go which is the main server for all connection and Port

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,5 +1,30 @@
-package main 
-import ("fmt")
+package main
+
+import (
+	"backend/internal/config"
+	"backend/internal/database"
+	"backend/internal/server"
+	"context"
+	"log"
+)
+
 func main (){
-	fmt.Println("Hello world")
+	cfg,err:=config.Load()
+	if err !=nil{
+		log.Fatal("Load config:",err)
+	}``
+	ctx:=context.Background()
+	db,err:=database.NewPool(ctx,cfg.DatabaseURL)
+	if err !=nil{
+		log.Fatalf("Connected to Database",err)
+	}
+	//close the db connection
+	defer db.Close()
+	//create the server  which is the main server
+	app := server.New(cfg, db)
+	log.Printf("Server runnig on https://localhost:%s",cfg.Port)
+	//listen the port 
+	if err := app.Listen(":"+cfg.Port); err != nil {
+		log.Fatalf("Listen : %v", err)
+	}
 }
