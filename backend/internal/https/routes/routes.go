44881f93e73fa71b841b288c8290c2714fdba972
Ankit-cs <ankitcareer018@gmail.com>
2026-05-28 17:43:31 +0530

    feat:solve all the bugs related to imports and versions and now fully bug free

--- backend/internal/https/routes/routes.go
+++ backend/internal/https/routes/routes.go
@@ -2,19 +2,19 @@ package routes
 
 import (
 	"backend/internal/https/handlers"
-	"backend/internal/https/middleware"
+	"backend/internal/https/middlewares"
 	"github.com/gofiber/fiber/v3"	
 )
 
 type RouteDependencies struct{
 	AuthHandler *handlers.AuthHandler
-	AuthMiddleware *middleware.AuthMiddleware
+	AuthMiddleware *middlewares.AuthMiddleware
 }
 
 func Register(app *fiber.App, deps RouteDependencies){
 	auth:=app.Group("/auth")
-	auth.Gt("/google",deps.AuthHandler.StartGoogleAuth)
+	auth.Get("/google",deps.AuthHandler.StartGoogleAuth)
 	auth.Get("/google/callback",deps.AuthHandler.GoogleAuthCallback)
-	auth.Post("/logout",deps.AuthHandler.Logout())
+	auth.Post("/logout",deps.AuthHandler.Logout)
    auth.Get("/me",deps.AuthMiddleware.RequireAuth(),deps.AuthHandler.GetUserInfo)
 }
