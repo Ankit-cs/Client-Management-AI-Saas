5fe1f3cda79381ff138a24b5857a2710b420153b
Ankit-cs <ankitcareer018@gmail.com>
2026-05-28 16:45:12 +0530

    updated fiber v2->v3 solve bug related to it

--- backend/internal/server/app.go
+++ backend/internal/server/app.go
@@ -3,39 +3,43 @@ package server
 import (
 	"backend/internal/config"
 	"backend/internal/https/handlers"
+	"backend/internal/https/middlewares"
+	"backend/internal/https/routes"
+	"backend/internal/repositories"
+	"backend/internal/services"
 	"strings"
 
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	recovermw "github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	recovermw "github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"golang.org/x/oauth2/authhandler"
 )
-func new(Cfg config.Config,db *pgxpool.Pool)*fiber.App{
-	app:=fiber.New()
+
+func New(cfg config.Config, db *pgxpool.Pool) *fiber.App {
+	app := fiber.New()
 
 	app.Use(logger.New())
-//now we have to create midddleware->fiber recover for handling any panic we canno use recover which is builtin function 
+	// now we have to create midddleware->fiber recover for handling any panic we canno use recover which is builtin function 
 	app.Use(recovermw.New())
-	app.Use(cors.New(cors.Config){
-		AllowOrigins:"*" || []string{cfg.FRONTEND_URL},
-		AllowMethods:[]string{"GET","POST","PUT","DELETE","OPTIONS","PATCH"},
-		AllowHeaders:[]string{"Origin","Content-Type","Accept","Authorization"},
-		AllowCredentials:true,
-		
-	})
-	//intiate our user repo
-	userRepo:=repositories.NewUserRepository(db)
-    authService:=services.NewAuthService(cfg)
-	//initate handler
-	authHandler:=handlers.AuthHandler(cfg,authService,userRepo)
-	authMiddleware:=middleware.NewAuthMiddleware(cfg,authService,userRepo)
-	routes.Register(app,routes.RouteDependecies{
-		AuthHandler:authHandler,
-		AuthMiddleware:authMiddleware,
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{cfg.FrontendURL},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+	// intiate our user repo
+	userRepo := repositories.NewUserRepository(db)
+	authService := services.NewAuthService(cfg)
+	// initate handler
+	authHandler := handlers.NewAuthHandler(cfg, authService, userRepo)
+	authMiddleware := middlewares.NewAuthMiddleware(cfg, authService, userRepo)
+
+	routes.Register(app, routes.RouteDependencies{
+		AuthHandler:    authHandler,
+		AuthMiddleware: authMiddleware,
 	})
 
 	return app
-	
-
 }
